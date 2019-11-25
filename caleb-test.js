73452c24ac67fb673851034ae78adfb5e05aad50
laxatzi <laxatzi@yahoo.com>
2019-11-25 11:29:23 +0200

    add javascript code to video player functionality

--- caleb-test.js
+++ caleb-test.js
@@ -1,1 +1,18 @@
+//code for my video player
+let video = document.getElementById('video1');
 
+function playOrPause(){
+   if (video.paused){
+      video.play();
+   }else{
+         video.pause();
+   }
+}
+
+function normalScreen(){
+   video.width= 420;
+}
+
+function bigScreen(){
+   video.width = 700;
+}
