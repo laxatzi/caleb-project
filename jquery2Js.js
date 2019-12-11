b932a9000caff0b3156067c7a9ed4cb989108a16
laxatzi <laxatzi@yahoo.com>
2019-12-11 13:34:07 +0200

    replace jquery focus and blur with vanilla js code

--- jquery2Js.js
+++ jquery2Js.js
@@ -1,3 +1,17 @@
-function myFunction() { 
+// focus() 
+/*
+    $("#focus").focus();
+*/
+// jquery focus() in vanilla js
+function setFocus() { 
    document.getElementById("focus").focus(); 
 } 
+
+// blur() 
+/*
+    $("#focus").blur();
+*/
+// jquery blur() in vanilla js
+function removeFocus() { 
+   document.getElementById("focus").blur(); 
+}
