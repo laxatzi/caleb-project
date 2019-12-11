bacfe971a48bf80831f84463766f0ee58dee3d95
laxatzi <laxatzi@yahoo.com>
2019-12-11 15:34:18 +0200

    replace jquery hover method with vanilla js

--- jquery2Js.js
+++ jquery2Js.js
@@ -15,3 +15,28 @@ function setFocus() {
 function removeFocus() { 
    document.getElementById("focus").blur(); 
 }
+
+
+// hover()
+/*
+    $(#box).hover(
+      function(){
+        // the effect to take place when the mouse is over the element
+      },
+      function(){
+        // the effect to take place when the mouse is NOT over the element
+      }
+    );
+*/
+
+// jquery hover() in vanilla js
+
+const greyBox = document.querySelector("#box");
+
+greyBox.addEventListener("mouseover", event => {
+  console.log("Mouse in");
+});
+
+greyBox.addEventListener("mouseout", event => {
+  console.log("Mouse out");
+});
