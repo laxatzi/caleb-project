610bba836cc402f8a053a18fce5a6cfd76851cd6
laxatzi <laxatzi@yahoo.com>
2019-12-13 10:59:42 +0200

    activate first slide

--- vanillaSlide2.js
+++ vanillaSlide2.js
@@ -7,5 +7,9 @@ if(slideshow != null){
      let currentSlide = 0; // starting place for the current slide (first position in that case)
      let slideHeight = null; // we gonna need this later for height calc
      let initHeight = slides[0].clientHeight; // find the height of the first slide
+
+     // let's activate the first slide on page load and set the height of that slide to the slideshow itself
+     slides[0].classList.add('active');
+     slideshow.style.height = initialHeight + "px";
 }
 
