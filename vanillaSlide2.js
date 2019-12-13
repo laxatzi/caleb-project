6344377c924663b806ecb611602676eaccd4740b
laxatzi <laxatzi@yahoo.com>
2019-12-13 10:23:31 +0200

    comment out variable setting

--- vanillaSlide2.js
+++ vanillaSlide2.js
@@ -2,10 +2,10 @@ const slideshow = document.querySelector(".slide_wrap");
 //make sure we don't run this script if the slideshow is not present...This is something we should always be doing when coding: preparing for the worst case scenario.
 if(slideshow != null){
    // Setting up our variables
-     let slides = document.querySelectorAll(".slide_entry");
-     let slideCount = slides.length;
-     let currentSlide = 0;
-     let slideHeight = null;
-     let initHeight = slides[0].clientHeight;
+     let slides = document.querySelectorAll(".slide_entry"); // grab all files
+     let slideCount = slides.length; // count number of slides in slider
+     let currentSlide = 0; // starting place for the current slide (first position in that case)
+     let slideHeight = null; // we gonna need this later for height calc
+     let initHeight = slides[0].clientHeight; // find the height of the first slide
 }
 
