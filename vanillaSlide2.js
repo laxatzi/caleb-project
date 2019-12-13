673708a730a097caa4d07f270b56440d30a38c44
laxatzi <laxatzi@yahoo.com>
2019-12-13 10:21:09 +0200

    set up conditions and variables in vanillaSlide2.js file

--- vanillaSlide2.js
+++ vanillaSlide2.js
@@ -1,1 +1,11 @@
+const slideshow = document.querySelector(".slide_wrap");
+//make sure we don't run this script if the slideshow is not present...This is something we should always be doing when coding: preparing for the worst case scenario.
+if(slideshow != null){
+   // Setting up our variables
+     let slides = document.querySelectorAll(".slide_entry");
+     let slideCount = slides.length;
+     let currentSlide = 0;
+     let slideHeight = null;
+     let initHeight = slides[0].clientHeight;
+}
 
