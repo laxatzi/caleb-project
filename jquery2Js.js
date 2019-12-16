9cb0668c5bc6979ffd44d98b52f2bb3dcf812de9
laxatzi <laxatzi@yahoo.com>
2019-12-16 16:25:26 +0200

    update jquery2js.js file

--- jquery2Js.js
+++ jquery2Js.js
@@ -1,71 +1,72 @@
 //FOCUS
-// focus() jquery 
-/*
-    $("#focus").focus();
-*/
-// jquery focus() in vanilla js
-function setFocus() { 
-   document.getElementById("focus").focus(); 
-} 
+  // focus() jquery 
+  /*
+      $("#focus").focus();
+  */
+  // jquery focus() in vanilla js
+  function setFocus() { 
+    document.getElementById("focus").focus(); 
+  } 
 
 //BLUR
-// blur()  jquery
-/*
-    $("#focus").blur();
-*/
-// jquery blur() in vanilla js
-function removeFocus() { 
-   document.getElementById("focus").blur(); 
-}
+  // blur()  jquery
+  /*
+      $("#focus").blur();
+  */
+  // jquery blur() in vanilla js
+  function removeFocus() { 
+    document.getElementById("focus").blur(); 
+  }
 
 
 // HOVER
-// hover() jquery
-/*
-    $(#box).hover(
-      function(){
-        // the effect to take place when the mouse is over the element
-      },
-      function(){
-        // the effect to take place when the mouse is NOT over the element
-      }
-    );
-*/
+  // hover() jquery
+  /*
+      $(#box).hover(
+        function(){
+          // the effect to take place when the mouse is over the element
+        },
+        function(){
+          // the effect to take place when the mouse is NOT over the element
+        }
+      );
+  */
 
 // jquery hover() in vanilla js
 
-const greyBox = document.querySelector("#box");
+  const greyBox = document.querySelector("#box");
 
-greyBox.addEventListener("mouseover", event => {
-  console.log("Mouse in");
-});
+  greyBox.addEventListener("mouseover", event => {
+    console.log("Mouse in");
+  });
 
-greyBox.addEventListener("mouseout", event => {
-  console.log("Mouse out");
-});
+  greyBox.addEventListener("mouseout", event => {
+    console.log("Mouse out");
+  });
 
 // TOGGLE
-/*
-function toggleFunction(){
-  $(".myDIV").toggleClass("toggleDiv");
-});
-*/
-// jquery hover() in vanilla js
-function toggleFunction() {
-  const element = document.querySelector(".myDIV");
-  element.classList.toggle("toggleDiv");
-}
+  /*
+  function toggleFunction(){
+    $(".myDIV").toggleClass("toggleDiv");
+  });
+  */
+
+// jquery toggle() in vanilla js
+  function toggleFunction() {
+    const element = document.querySelector(".myDIV");
+    element.classList.toggle("toggleDiv");
+  }
 
 //  CSS STYLE ELEMENTS
-// With jQuery
+  // With jQuery
 
-  /* Pass multiple styles
-  $(".box2").css({
-    "color": "#000",
-    "background-color": "#f00"
-  });
-*/
-// Without jQuery
+    /* Pass multiple styles
+    $(".box2").css({
+      "color": "#000",
+      "background-color": "#f00"
+    });
+  */
+  // Without jQuery
 
   // Set color to #000 and background to #f00
 
@@ -76,43 +77,48 @@ function toggleFunction() {
   // Set all styles at once (and override any existing styles)
   box2.style.cssText = "color: #000; background-color: #f00";
 
-  //CLICK
+   // Get Style
+      // in jQuery 
+      $(el).css(ruleName);
+      // without jQuery
+      getComputedStyle(element)[ruleName];
+//CLICK
+    // with jquery
+    /*
+    $(".btn").click(function(e) { handle click event });
+    */
+  //without jquery
+  // document.querySelector(".btn").addEventListener("click", (e)=> { /* handle event listener */ });
+
+// events with keys
+    /*
+    .keyup()
+    .keydown()
+    .keypress()
+    */
   // with jquery
   /*
-  $(".btn").click(function(e) { handle click event });
+  $(document).keyup(function(e) { handle keyup event });
   */
- //without jquery
- // document.querySelector(".btn").addEventListener("click", (e)=> { /* handle event listener */ });
+  // without jquery
+  document.addEventListener("keyup", (e)=> { /* handle keyup event*/ });
 
-  // events with keys
-  /*
-  .keyup()
-  .keydown()
-  .keypress()
-  */
- // with jquery
- /*
- $(document).keyup(function(e) { handle keyup event });
-*/
-// without jquery
-document.addEventListener("keyup", (e)=> { /* handle keyup event*/ });
-
- // mouse events
- /*
-    .mouseup();
-    .mousedown();
-    .mouseover();
-    .mouseenter();
-    .mouseleave();
-    .mouseover();
-    .mouseout();
- */
-// with jQuery
-/*
-$(".button").mouseenter(function(e) { handle event listener });
-*/
-// without jquery
-//document.querySelector(".button").addEventListener("mouseenter", (e)=> { /*handle event listener*/ });
+// mouse events
+    /*
+        .mouseup();
+        .mousedown();
+        .mouseover();
+        .mouseenter();
+        .mouseleave();
+        .mouseover();
+        .mouseout();
+    */
+    // with jQuery
+    /*
+    $(".button").mouseenter(function(e) { handle event listener });
+    */
+    // without jquery
+    //document.querySelector(".button").addEventListener("mouseenter", (e)=> { /*handle event listener*/ });
 
 // HIDE & SHOW
   // with jquery
@@ -125,56 +131,53 @@ $(".button").mouseenter(function(e) { handle event listener });
   document.querySelector(".class").style.display= "none";
   document.querySelector(".class").style.display = "block";
 */
-  // FADEIN -- FADEOUT
 
-// With jQuery
-/*
-$(".btn1").click(function(){
-  $(".pink").fadeOut();
-});
-$(".btn2").click(function(){
-  $("pink").fadeIn();
-});
-*/
+// FADEIN -- FADEOUT
 
-// without jQuery
-const pink = document.querySelector(".pink");
-function fadeout(){
-   pink.classList.add("fadeout_hide");
-   pink.classList.remove("fadeout_show");
-}
-function fadein(el){
-  pink.classList.add("fadein_show");
-  pink.classList.remove("fadeout_hide");
-}
+    // With jQuery
+    /*
+    $(".btn1").click(function(){
+      $(".pink").fadeOut();
+    });
+    $(".btn2").click(function(){
+      $("pink").fadeIn();
+    });
+    */
 
-// SLIDE
-
-// HTML
+    // without jQuery
+    const pink = document.querySelector(".pink");
+    function fadeout(){
+      pink.classList.add("fadeout_hide");
+      pink.classList.remove("fadeout_show");
+    }
+    function fadein(el){
+      pink.classList.add("fadein_show");
+      pink.classList.remove("fadeout_hide");
+    }
 
 // TEXT OF AN ELEMENT ... read(get) or update(set)
 
-//with jquery
-/*
-   set button text -- $('.button').text("some new text");
-   read(get) button text -- $(".button").text();
+  //with jquery
+  /*
+    set button text -- $('.button').text("some new text");
+    read(get) button text -- $(".button").text();
 
-*/
+  */
 
-// without jquery  *(use of innerHTML not advisable for safety reasons regarding this purpose)
-document.querySelector(".button").textContent = "some text"; // set
-document.querySelector(".button").textContent; // get
+  // without jquery  *(use of innerHTML not advisable for safety reasons regarding this purpose)
+  document.querySelector(".button").textContent = "some text"; // set
+  document.querySelector(".button").textContent; // get
 
 // CONSTRUCTION OF AN ELEMENT
  
-// with jquery
-/*
-  --create div and append it to .container
-   $(".container").append($("<div></div>"));
-*/
-// without jquery 
-const element = document.createElement("div");
-document.querySelector(".container").appendChild(element);
+  // with jquery
+  /*
+    --create div and append it to .container
+    $(".container").append($("<div></div>"));
+  */
+  // without jquery 
+  const element = document.createElement("div");
+  document.querySelector(".container").appendChild(element);
 
 // Put together, here’s how to create a div, update its text and class, and add it to the DOM:
 
@@ -192,3 +195,25 @@ document.querySelector(".container").appendChild(element);
     const unorderedList = document.querySelector("#list");
     unorderedList.insertAdjacentHTML("beforeend", `<li>list item</li>`);
 */
+
+//Classes
+    // with jQuery
+    // Add, remove, toggle the 'focus' class
+    $('.box').addClass('focus');
+    $('.box').removeClass('focus');
+    $('.box').toggleClass('focus');
+
+    // WITHOUT jQuery
+    const box = document.querySelector('.box');
+    box.classList.add('focus');
+    box.classList.remove('focus');
+    box.classList.toggle('focus');
+
+   //If you want to remove or add multiple classes you can just pass multiple arguments to .add() and .remove():
+
+   box.classList.add('focus', 'highlighted');
+   box.classList.remove('focus', 'highlighted');
+
+   //Checking if an element has a class
+
+  
--- vanillaSlide2.js
+++ vanillaSlide2.js
@@ -1,16 +1,16 @@
-const slide_show = document.querySelector(".slide_wrap");
+const slideshow = document.querySelector(".slide_wrap");
 //make sure we don't run this script if the slideshow is not present...This is something we should always be doing when coding: preparing for the worst case scenario.
-if(slide_show != null){
+if(slideshow != null){
    // Setting up our variables
      let slides = document.querySelectorAll(".slide_entry"); // grab all files
      let slideCount = slides.length; // count number of slides in slider
      let currentSlide = 0; // starting place for the current slide (first position in that case)
-     let slideHeight = null; // we gonna need this later for height calc
+     let slideHeight = null; // we gonna need this later for height calc so we declare it now
      let initHeight = slides[0].clientHeight; // find the height of the first slide
 
      // let's activate the first slide on page load and set the height of that slide to the slideshow itself
      slides[0].classList.add('active');
-     slide_show.style.height = initHeight + "px";
+     slideshow.style.height = initHeight + "px";
 
      // next/prev functionality 
      function slideNavigation(n) { 
@@ -18,11 +18,11 @@ if(slide_show != null){
          currentSlide = (n + slideCount) % slideCount; // determine current slide
          slides[currentSlide].className = 'slide_entry active'; // if its current slide add active class
          slideHeight = slides[currentSlide].clientHeight; // get the height of the current slide
-         slide_show.style.height = slideHeight + "px"; // set the height of the slides
+         slideshow.style.height = slideHeight + "px"; // set the height of the slides
          window.addEventListener('resize', function(){ // if browser resizes
-            resizedSlideHeight = slides[currentSlide].clientHeight; // get current slide height
-            slide_show.style.height = resizedSlideHeight + "px"; // update height of slideshow
-         });
+         let resizedSlideHeight = slides[currentSlide].clientHeight; // get current slide height
+         slideshow.style.height = resizedSlideHeight + "px"; // update height of slideshow
+      });
      }
 
 // ad navigational functions
