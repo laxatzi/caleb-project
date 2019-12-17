59f7dc9140919f292eaeda644ae134375e782ddc
laxatzi <laxatzi@yahoo.com>
2019-12-17 19:43:54 +0200

    update jquery2js.js file

--- jquery2Js.js
+++ jquery2Js.js
@@ -295,6 +295,7 @@
    while(element.firstChild)
     element.removeChild(element.firstChild);
     
+    
 //Event listening for dynamically added elements aka... on() method
    //jQuery’s .on() method enables you to work with “live” event handlers, where you listen to events on objects that get dynamically added to the DOM.
 
