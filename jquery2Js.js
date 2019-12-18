7e620b03ac0dc087c3c3d9f1a649b1ced29eca63
laxatzi <laxatzi@yahoo.com>
2019-12-18 09:33:35 +0200

    update jquery2js.js file

--- jquery2Js.js
+++ jquery2Js.js
@@ -305,7 +305,6 @@
      node.insertBefore(newnode, existingnode)
 
 
-
 //Event listening for dynamically added elements aka... on() method
    //jQuery’s .on() method enables you to work with “live” event handlers, where you listen to events on objects that get dynamically added to the DOM.
 
