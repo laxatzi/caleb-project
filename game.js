a8f025edd3ef4f291a7f856e50c040c0f72032f6
laxatzi <laxatzi@yahoo.com>
2019-12-23 13:00:24 +0200

    storing all quiz info into an object

--- game.js
+++ game.js
@@ -1,10 +1,10 @@
-var quizGame =  [
-   ["How many times was PAOK nominated 'championship winner'?", "3"],
-   ["Who is team's captain as of 2019?", "Andre Vierihnia"],
-   ["Where is PAOK's home stadium located?", "Toumba, Thessaloniki"]
-];
+// var quizGame =  [
+//    ["How many times was PAOK nominated 'championship winner'?", "3"],
+//    ["Who is team's captain as of 2019?", "Andre Vierihnia"],
+//    ["Where is PAOK's home stadium located?", "Toumba, Thessaloniki"]
+// ];
 
-var score = 0;
+// var score = 0;
 /*
    OLD CODE
 for(var i=0; i<quizGame.length; i++){
@@ -24,21 +24,32 @@ alert("Game is over, you scored "+score+" points!");
   // Refactoring is the process of improving the code's structure and maintainability without changing it's behaviour
 
   // We create a main function that contains all the steps of playing the game
- 
+ var quizGame = {
+    "name": "Super Hero Name quiz game",
+    "description": "How many super heroes can you name?",
+    "question": "What is the real name of ",
+    "questions": [
+       {"question": "Superman", "answer", "Clark Kent"},
+       {"question" : "Batman", "answer", "Bruce Wayne"},
+       {"question": "Wonder Woman", "answer", "Dianna Pierce"}
+    ]
+ }
+
   function play(quizGame){ // we insert the quizGame arr as an argument
      // main game loop
      for(var i=0; i<quizGame.length; i++){
-      var question = quizGame[i][0];
+      var question = quizGame.questions[i].question;
       var answer = ask(question);
       check(answer);
       
    }
+   // my functions
    function ask(question){
-      return prompt(question);
+      return prompt(quiz.question + question);
    }
  
    function check(answer){
-    if(answer === quizGame[i][1]){
+    if(answer === quizGame.questions[i].answer){
        alert("Bravo. You earn one point!");
        score++;
     }else{
@@ -49,8 +60,8 @@ alert("Game is over, you scored "+score+" points!");
   function gameOver(){
     alert("Game is over, you scored "+score+" points!");
  }
-   // end of main loop
+   
    gameOver();
-  }
+  } // and play function
 
-play(quizGame);
+play(quizGame); // execute main function 
