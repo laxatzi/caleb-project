c1d8c8bfe2978c44523cb05ffbcbf906c8230054
laxatzi <laxatzi@yahoo.com>
2019-12-30 12:11:40 +0200

    clean code in game.html and game.js files

--- game.js
+++ game.js
@@ -1,73 +1,79 @@
-//function to update elements on the page
-function update(element, content, klass){ // first param is the element to be updated,
-                                          // second param is the content that is to be updated with
-                                          // third param is an optional class 
-   var p = element.firstChild || document.createElement("p"); // if element already has a first child assign it to p variable. If not already, create one.
-   p.textContent = content; // Add the second argument as the content of the element
-   element.appendChild(p); // insert p inside the element
-   if(klass){ // if klass argument exists use to p 
-     p.className = klass;
-   }
-}
-
- var quizGame = {
-    "name": "Super Hero Name quiz game",
-    "description": "How many super heroes can you name?",
-    "question": "What is the real name of ",
-    "questions": [
-       {
-         "question": "Superman",
-         "answer" : "Clark Kent"
-      },
-       {
-         "question" : "Batman",
-         "answer":"Bruce Wayne"
-      },
+var quizGame = {
+   "name": "Super Hero Name quiz game",
+   "description": "How many super heroes can you name?",
+   "question": "What is the real name of ",
+   "questions": [
       {
-         "question": "Wonder Woman",
-         "answer": "Dianna Pierce"
-      }
-    ]
- }
- var score = 0;
-  // DOM references
-  var $question = document.getElementById('question');
+        "question": "Superman",
+        "answer" : "Clark Kent"
+     },
+      {
+        "question" : "Batman",
+        "answer":"Bruce Wayne"
+     },
+     {
+        "question": "Wonder Woman",
+        "answer": "Dianna Pierce"
+     }
+   ]
+};
+
+// DOM references
+  var $question = document.getElementById('questionSection');
   var $feedback = document.getElementById('feedback');
   var $score = document.getElementById('score');
-  
 
-  // A main function that contains all the steps of playing the game
-  function play(quizGame){ // we insert the quizGame arr as an argument
-     // main game loop
-     update($score, score); // display score into header
+/// My Functions ///
 
-     for(var i=0; i<quizGame.questions.length; i++){
-      var question = quizGame.questions[i].question;
-      var answer = ask(question);
-      check(answer);
+      //function to update elements on the page
+      function update(element, content, klass){ // first param is the element to be updated,
+                                                // second param is the content that is to be updated with
+                                                // third param is an optional class 
+         var p = element.firstChild || document.createElement("p"); // if element already has a first child assign it to p variable. If not already, create one.
+         p.textContent = content; // Add the second argument as the content of the element
+         element.appendChild(p); // insert p inside the element
+         if(klass){ // if klass argument exists use to p 
+         p.className = klass;
+         }
+      }
+
+
+      var score = 0;
+
       
-   }
-   // my functions
-   function ask(question){ // the question is displayed into the html instead of the dialogue
-      update($question, quizGame.question + question);
-      return prompt("Enter your answer:");
-   }
- 
-   function check(answer){
-    if(answer === quizGame.questions[i].answer){
-       update($feedback,"Bravo. You earn one point!", "right" ); // add a 3rd arg to style as we wish
-       score++;
-       update($score, score);
-    }else{
-       update($feedback, "Sorry, your answer is wrong!", "wrong"); // add a 3rd arg to style as we wish
-    }
-   }
- 
-  function gameOver(){
-     update($question, "Game is over, you scored "+score+" points!");
- }
-   
-   gameOver();
-  } // and play function
 
-play(quizGame); // execute main function 
+      // A main function that contains all the steps of playing the game
+      function play(quizGame){ // we insert the quizGame arr as an argument
+         // main game loop
+         update($score, score); // display score into header
+
+         for(var i=0; i<quizGame.questions.length; i++){
+            var question = quizGame.questions[i].question;
+            var answer = ask(question);
+            check(answer);
+         }
+      
+         // my functions
+         function ask(question){ // the question is displayed into the html instead of the dialogue
+            update($question, "Whats up duck?", "question");
+            //return prompt("Enter your answer:");
+         }
+      
+         function check(answer){
+         if(answer === quizGame.questions[i].answer){
+            update($feedback,"Bravo. You earn one point!", "right" ); // add a 3rd arg to style as we wish
+            score++;
+            update($score, score);
+         }else{
+            update($feedback, "Sorry, your answer is wrong!", "wrong"); // add a 3rd arg to style as we wish
+         }
+         }
+      
+      function gameOver(){
+         update($question, "Game is over, you scored "+score+" points!");
+         }
+         
+         gameOver();
+      } // and play function
+
+      play(quizGame); // execute main function 
