b71a27906354279dd8cb0835a20a1fcb649acc8e
laxatzi <laxatzi@yahoo.com>
2020-02-20 13:10:07 +0200

    update testingPage.js file and create captures.js file

--- testingPage.js
+++ testingPage.js
@@ -1,2 +1,21 @@
+// dom refs
+   const $div = document.getElementById('div');
 
-function update()
+// function definitions
+function update(elem, content, clas){
+   console.log("update() invoked");
+    let p = document.createElement('p');
+    p.textContent = content;
+    elem.appendChild(p);
+
+    if(clas){ p.className = clas;}
+}
+function hide(elem){
+   elem.style.display = "none";
+}
+function show(elem){
+   elem.style.display = "block";
+}
+
+update($div,"hello world","distinct" );
+
