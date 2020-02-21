9c2d284a951c7a627a4ccf066f8c9f76f6ff36da
laxatzi <laxatzi@yahoo.com>
2020-02-21 11:08:02 +0200

    elaborate on the consept of immutability with examples

--- funprog.js
+++ funprog.js
@@ -1,4 +1,6 @@
 // Intro to Functional Programming Concepts
+// Elaborate on the Matt Banz article on opensource.com/articles 
+
 /* 
    Javascript is a multi paradigm language that allows you to freely mix and match OOP, Functional, and Procedural paradigms. 
    Recently there 's been a growing trend towards functional programming. 
@@ -70,5 +72,101 @@ function multiply(a, b) {
 
      IMMUTABILITY
 
+      Returning on the concept of captured variables, we looked at the canRide function and concluded that it is an impure function since the heightRequirement can be reassigned. 
+      It should be emphasized that captured variables do not necessary make a function impure. That is depend on the declaration of it.
+      If keyword 'const' is used, meaning that variable cannot be reassigned, then the above function would considered pure.
+      But what if instead of a number we had an object that stored all our constants?
      
 */
+  const constants = {
+    heightRequirement : 46,
+    ageRequirement: 8,
+    parentRequirement: 'Being present',
+  }
+
+  function canRide(height){
+    return height >= constants.heightRequirement;
+  }
+
+  /* 
+      We used 'const' so that the variable cannot be reassigned BUT the object can be MUTATED.
+      We need data structures to be immutable.
+      To prevent mutation the JS language provides as with the Object.freeze method.
+  */
+
+  (function(){
+    'use strict';
+
+// CASE 1: The object is mutable and the variable can be reassigned
+    let obj1 = { foo: 'bar' };
+    // Mutate the object
+    obj1.foo = "not bar anymore";
+    // Reassign the variable
+    obj1 = { message: "This is a new object"};
+
+// CASE 2: The object is mutable but the variable cannot be reassigned
+    const obj2 = { foo: 'baz' };
+    // Mutate the object 
+    obj2.foo = "not baz anymore";
+    // Reassigning the variable not possible 
+    //obj2 = { message: "New object will create error"}; // Error...
+
+// CASE 3: The object is immutable but the variable CAN be reassigned
+   let obj3 = Object.freeze({ foo: 'Cannot be mutated due to the Object.freeze method implemented on me!'});
+   //obj3.foo = "unable to mutate the original foo"; // Error...
+
+   // Can still reassign the variable
+   obj3 = { message: "i am a new object"};
+
+// CASE 4: The object is immutable and the variable cannot be reassigned
+    const obj4 = Object.freeze({ foo: 'I\'not going to change, ever!'});
+    // Cannot mutate the object
+    // obj4 = "I will chang you someday!"; //Error...
+
+    // Cannot reassign the variable
+    // obj4 = { message: "i am a new object" }; // Error...
+
+  }());
+
+  /* 
+   IMMUTABILITY is applicable to all data structures including arrays, maps and sets. That means we cannot call mutator methods (array.prototype.unshift). 
+   We can create a new array with the same items as the original and augment the original item instead.
+   In fact, EVERY mutator method can be replaced by a function that returns a new array with the desired changes.
+  */
+
+  (function(){
+    "use strict";
+ function myFunc(){ return true};
+
+    const numbs = Object.freeze([3,4,5]);
+
+    // instead of numbs.push(6);
+    const numbs2 = numbs.concat(6);
+
+    // instead of numbs.pop();
+    const numbs3 = numbs.slice((0,-1));
+
+    // instead of numbs.unshift(1,2);
+    const numbs4 = (1,2).concat(numbs);
+
+    // instead of numbs.shift();
+    const numbs5 = numbs.slice(0,1);
+
+    // instead of numbs.sort(myFunc, numbs);
+    const numbs6 = R.sort(myFunc,numbs); // R = Ramda library
+
+    // instead of numbs.reverse(numbs);
+    const numbs7 = R.reverse(numbs); // R = Ramda library
+
+  }());
+
+  /* 
+        EXERCISE FOR THE READER:
+        copyWithin
+        fill
+        splice
+
+        The same thing goes when using a Map or a Set. We can avoid mutator methods by returning a new Map or Set with the desired changes.
+        Sometimes creating new objects instead of mutating existing ones can affect performance (depending on the project). 
+        In that case consider using immutable.js 
+  */
